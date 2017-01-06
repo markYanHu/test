af876aeed3bcc1a42dde5ec6bebd316432b05694
yh_mark <18380122117@163.com>
2017-01-06 17:13:12 +0800

    yyufyf

---
+++ testOnly/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testOnly
+//
+//  Created by 锐智互动 on 17/1/6.
+//  Copyright © 2017年 锐智互动. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testOnly/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  testOnly
+//
+//  Created by 锐智互动 on 17/1/6.
+//  Copyright © 2017年 锐智互动. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
